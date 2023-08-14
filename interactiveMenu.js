7d31073692eec6e65f292d4400d82c9bf1213007
LucasVidalle <lucas.vidalle@gm2dev.com>
2023-08-14 12:20:27 -0300

    feat(interactiveMenu.js):finish exercise

--- interactiveMenu.js
+++ interactiveMenu.js
@@ -1,53 +1,51 @@
+let products = [];
+
 const User = {
-    Username: "User98",
-    Password: "1234",
-  }
-  
-  function login() {
-    for (let intentos = 1; intentos <= 3; intentos++) {
-      const Username = prompt("enter your username");
-      const Password = prompt("enter your password");
-  
-      console.log(Username);
-      console.log(Password);
-  
-      if (Username == User.Username && Password == User.Password) {
-        console.log("logged in");
-        return true;
-      }
-      else {
-        console.log("attempt failed, try again")
-      }
+  Username: "1234",
+  Password: "1234",
+}
+
+function login() {
+  for (let intentos = 1; intentos <= 3; intentos++) {
+    const Username = prompt("enter your username");
+    const Password = prompt("enter your password");
+
+    console.log(Username);
+    console.log(Password);
+
+    if (Username == User.Username && Password == User.Password) {
+      console.log("logged in");
+      return true;
     }
-    return false;
-  };
-  
- main();   
+    else {
+      console.log("attempt failed, try again")
+    }
+  }
+  return false;
+};
+
+main();
 
 function mainMenu() {
-  const menu = prompt (`Menú
+  const menu = prompt(`Menú
     [0] - Create a product
     [1] - Show products
     [2] - Go out`
-    );
+  );
 
-    if (menu == 0){
-      prompt ("Enter the product title")
-      prompt ("Enter the product description")
-      prompt ("Enter the product price")
-    }    
-    
-    if (menu == 1){
-      alert ("List of products")
-      console.log (products);
-    }
+  if (menu == 0) {
+    createProduct();
+  }
 
-    if (menu == 2){
-      return false
-    }
-  }  
+  if (menu == 1) {
+    alert("List of products")
+    console.log(products);
+  }
 
-
+  if (menu == 2) {
+    return false
+  }
+}
 
 function main() {
   if (login() == true) {
@@ -55,29 +53,21 @@ function main() {
   }
 }
 
-let products = [];
+function createProduct() {
+  let product = {
+    title: prompt(`Enter the product title`),
+    description: prompt(`Enter the product description`),
+    price: prompt(`Enter the product price`),
+  };
+  uploadProducts(product)
+  console.log(products);
+}
 
-  function createProduct(title, description, price){
-      let product = {
-      title,
-      description,
-      price,
-      };
-
-      console.log (product);
-      return product;
-  }
-
-function uploadProducts(product, array){
-  array.push(product);
-  return array;
-}  
-
-console.log(products);
+function uploadProducts(product, array) {
+  products.push(product);
+  const productsJSON = JSON.stringify(products);
+  localStorage.setItem(`Products`, productsJSON);
+}
 
 
-localStorage.setItem(`Products`,products);
 
-const productsJSON = JSON.stringify(products);
-
-localStorage.setItem(`Products`, productsJSON);
